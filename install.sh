44e3f089f6d8fede1ccb44b1e42487a3223ec186
Victorrex04 <102107404+Victorrex04@users.noreply.github.com>
2026-06-17 12:58:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,1 @@
-git init
-git add install.sh
-git commit -m "add install script"
-git remote add origin https://github.com/und3rxpl0it/rexus-cor.git
-git push -u origin main
+curl -sL https://raw.githubusercontent.com/und3rxpl0it/rexus-cor/main/install.sh | bash  
